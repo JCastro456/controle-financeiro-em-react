445a0439dad546647d1d662e0187b7962edbaad6
Jackeline Castro <jackelinecastrocardoso@hotmail.com>
2023-06-16 14:21:40 -0300

    controle financeiro

---
+++ src/components/Header/style.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+   height: 150px;
+   text-align: center;
+   background: linear-gradient(180deg, #70b5ff 0, #86b1ff 8.33%, #99adff 16.67%, #aaa9ff 25%, #baa5ff 33.33%, #c8a1f9 41.67%, #d59df2 50%, #e099ea 58.33%, #eb96e1 66.67%, #f392d7 75%, #fa90cd 83.33%, #ff8ec2 91.67%, #ff8db7 100%);
+`;   
+
+export const Header = styled.h1``;
+
+export const Title = styled.div`
+   padding-top: 20px;
+   color: #fff;
+`;
